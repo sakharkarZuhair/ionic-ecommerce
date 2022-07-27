35dd311f14bca55e33de4a7bec2c62034d73af59
zuhair-credencesoft <zuhair@credencesoft.in>
2022-07-27 14:01:13 +0530

    react-router-dom navigation added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import "./Styles/App.css";
 import React from "react";
 import { IonApp } from "@ionic/react";
+import { Switch, Route } from "react-router-dom";
 // import LandingScreen from "./Screens/LandingScreen";
-// import LoginScreen from "./Screens/LoginScreen";
-// import SignupScreen from "./Screens/SignupScreen";
+import LoginScreen from "./Screens/LoginScreen";
+import SignupScreen from "./Screens/SignupScreen";
 import SuccessfulScreen from "./Screens/SuccessfulScreen";
+import HomeScreen from "./Screens/HomeScreen";
 
 const App = () => {
   return (
     <IonApp>
-      {/* <LandingScreen /> */}
-      {/* <LoginScreen /> */}
-      {/* <SignupScreen /> */}
-      <SuccessfulScreen />
+      <Switch>
+        {/* <LandingScreen /> */}
+        <Route path="/login" component={LoginScreen} />
+        <Route path="/signup" component={SignupScreen} />
+        <Route path="/successful" component={SuccessfulScreen} />
+        <Route path="/home" component={HomeScreen} />
+      </Switch>
     </IonApp>
   );
 };
