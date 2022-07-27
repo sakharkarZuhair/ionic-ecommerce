1a9be48c581b426547037476eaa32d40e511b69a
zuhair-credencesoft <zuhair@credencesoft.in>
2022-07-27 17:03:16 +0530

    HomeScreen Added.

--- src/Screens/HomeScreen.jsx
+++ src/Screens/HomeScreen.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { IonPage, IonGrid, IonRow, IonCol } from "@ionic/react";
+import { HiOutlineMenuAlt2, HiSearch } from "react-icons/hi";
+import { FiSearch } from "react-icons/fi";
+import "../Styles/HomeScreen.css";
 
 const HomeScreen = () => {
-  return <div>HomeScreen</div>;
+  return (
+    <IonPage>
+      <IonGrid>
+        <IonRow className="firstContainer">
+          <IonCol className="colOne">
+            <HiOutlineMenuAlt2 className="menuIcon" />
+          </IonCol>
+          <IonCol className="colTwo">
+            <div className="searchBox">
+              <input
+                className="searchText"
+                type="text"
+                placeholder="Type to search"
+              />
+              <FiSearch className="searchButton" />
+            </div>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+      {/* <div className="container">
+        <div>
+          <HiOutlineMenuAlt2 />
+        </div>
+        <h4>
+          <HiSearch />
+        </h4>
+      </div> */}
+    </IonPage>
+  );
 };
 
 export default HomeScreen;
