ab331296c9810dc43effb8ca07df1afc5665612c
zuhair-credencesoft <zuhair@credencesoft.in>
2022-08-17 15:50:46 +0530

    Get all Products route created.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ connectDB();
 
 const app = express();
 app.use(express.json()); //Body Parsing
+
+app.use("/api/products", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is Running");
